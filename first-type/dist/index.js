76b09f70bd9cb237d7c0a960791e96b475bdd67e
Salami Azeez <salamitopeazeez@gmail.com>
2024-03-05 09:07:54 -0800

    Arrays in typescript

--- first-type/dist/index.js
+++ first-type/dist/index.js
@@ -4,3 +4,6 @@ let id = 5;
 let company = "Tez-cyber";
 let isPublished = true;
 let x = "Hello";
+//Arrays
+let ids = [1, 2, 3, 4, 5];
+let arr = [1, true, "Hello"];
--- first-type/src/index.ts
+++ first-type/src/index.ts
@@ -3,3 +3,7 @@ let id: number = 5
 let company: string = "Tez-cyber"
 let isPublished: boolean = true
 let x: any = "Hello"
+
+//Arrays
+let ids: number[] = [1, 2, 3, 4, 5]
+let arr: any[] = [1, true, "Hello"]
