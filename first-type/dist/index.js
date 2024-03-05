0395441ec449a874e63b63c7833f9472c0ad46dd
Salami Azeez <salamitopeazeez@gmail.com>
2024-03-05 09:14:21 -0800

    Tuple and Tuple Array in typescript

--- first-type/dist/index.js
+++ first-type/dist/index.js
@@ -7,3 +7,12 @@ let x = "Hello";
 //Arrays
 let ids = [1, 2, 3, 4, 5];
 let arr = [1, true, "Hello"];
+//Tuple
+let person = [1, "Hello", true];
+// Tuple Arrays
+let employee;
+employee = [
+    [1, "Drex"],
+    [2, "Mason"],
+    [3, "Tez"]
+];
--- first-type/src/index.ts
+++ first-type/src/index.ts
@@ -7,3 +7,15 @@ let x: any = "Hello"
 //Arrays
 let ids: number[] = [1, 2, 3, 4, 5]
 let arr: any[] = [1, true, "Hello"]
+
+//Tuple
+let person: [number, string, boolean] = [1, "Hello", true]
+
+// Tuple Arrays
+let employee: [number, string][]
+
+employee = [
+    [1, "Drex"],
+    [2, "Mason"],
+    [3, "Tez"]
+]
